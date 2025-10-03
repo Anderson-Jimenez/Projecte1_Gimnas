23330c11224cb0f7e79822db3b6e71c09b9e24cd
Anderson-Jimenez <andersonstiven.jimenez@inslapineda.cat>
2025-10-03 13:08:52 +0200

    preparacio i configuració mysql connection

---
+++ src/main/java/com/mycompany/projecte1_gimnas/Models/DatabaseConnection.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.projecte1_gimnas.Models;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class DatabaseConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/gym";
+    private static final String USER = "root";
+    private static final String PASSWORD = "";
+
+    public static Connection getConnection() {
+        try {
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException e) {
+            System.out.println("Error al conectar a la base de datos: " + e.getMessage());
+            return null;
+        }
+    }
+}
--- src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module com.mycompany.projecte1_gimnas {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens com.mycompany.projecte1_gimnas to javafx.fxml;
-    exports com.mycompany.projecte1_gimnas;
-}
