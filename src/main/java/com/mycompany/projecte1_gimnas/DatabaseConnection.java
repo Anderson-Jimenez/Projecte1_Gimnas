cc02e749d64923486ce696eef126ebd066e1147c
Anderson-Jimenez <andersonstiven.jimenez@inslapineda.cat>
2025-10-03 13:21:44 +0200

    preparacio i configuració mysql connection

--- src/main/java/com/mycompany/projecte1_gimnas/DatabaseConnection.java
+++ src/main/java/com/mycompany/projecte1_gimnas/DatabaseConnection.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.mycompany.projecte1_gimnas.Models;
+package com.mycompany.projecte1_gimnas;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
--- src/main/java/com/mycompany/projecte1_gimnas/PrimaryController.java
+++ src/main/java/com/mycompany/projecte1_gimnas/PrimaryController.java
@@ -2,11 +2,20 @@ package com.mycompany.projecte1_gimnas;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 public class PrimaryController {
 
     @FXML
     private void switchToSecondary() throws IOException {
-        App.setRoot("secondary");
+        //App.setRoot("secondary");
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/mycompany/myapp/check-view.fxml"));
+        Stage stage = new Stage();
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Verificación de conexión");
+        stage.setScene(scene);
+        stage.show();
     }
 }
