0b7dbe264963511f0eed0c3c1049f4eaf8437f2b
roro333111 <arturbartres@gmail.com>
2025-10-17 10:22:30 +0200

    artur 17.10.2025 creacio de vistes main i edicio horaris

---
+++ src/main/java/com/mycompany/projecte1_gimnas/Edit_timetable_Controller.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.projecte1_gimnas;
+
+/**
+ *
+ * @author asix1
+ */
+public class Edit_timetable_Controller {
+    
+}
---
+++ src/main/java/com/mycompany/projecte1_gimnas/Main_panell_Controller.java
@@ -0,0 +1,61 @@
+package com.mycompany.projecte1_gimnas;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+
+public class Main_panell_Controller {
+
+    @FXML
+    private Button assignInstructorsBtn;
+
+    @FXML
+    private Button closeSessionBtn;
+
+    @FXML
+    private Button editTimeBtn;
+
+    @FXML
+    private AnchorPane mainBackground;
+
+    @FXML
+    private Button manageAppointmentsBtn;
+
+    @FXML
+    private Button manageClientsBtn;
+
+    @FXML
+    private Button showStatsBtn;
+
+    @FXML
+    void assignInstructors(ActionEvent event) {
+
+    }
+
+    @FXML
+    void closeSession(ActionEvent event) {
+
+    }
+
+    @FXML
+    void editTimetable(ActionEvent event) {
+
+    }
+
+    @FXML
+    void manageAppointments(ActionEvent event) {
+
+    }
+
+    @FXML
+    void manageClients(ActionEvent event) {
+
+    }
+
+    @FXML
+    void showStats(ActionEvent event) {
+
+    }
+
+}
