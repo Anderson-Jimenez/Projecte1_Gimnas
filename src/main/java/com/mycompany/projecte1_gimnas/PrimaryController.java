65995f9057a53c0d711d8126c5493e8435d7d24e
Anderson-Jimenez <andersonstiven.jimenez@inslapineda.cat>
2025-10-17 09:10:50 +0200

    creacio vista i proves controller login

--- src/main/java/com/mycompany/projecte1_gimnas/PrimaryController.java
+++ src/main/java/com/mycompany/projecte1_gimnas/PrimaryController.java
@@ -1,39 +1,44 @@
 package com.mycompany.projecte1_gimnas;
 
 import java.io.IOException;
-import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
+import java.net.URL;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+
+import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 
 public class PrimaryController {
 
     @FXML
-    private Button primaryButton;
+    private AnchorPane loginPanell;
 
     @FXML
-    private Label usernameLabel;
+    private PasswordField passwordInput;
+
+    @FXML
+    private TextField usernameInput;
 
     @FXML
     private void connectionButton() throws IOException, SQLException, ClassNotFoundException {
-        // Obtener la conexión desde tu clase DatabaseConnection
         Connection conn = DatabaseConnection.getConnection();
-
         String connectionQuery = "SELECT * FROM administrator";
-
+        
         try {
             if (conn != null) {
                 Statement statement = conn.createStatement();
                 ResultSet queryOutput = statement.executeQuery(connectionQuery);
 
                 while (queryOutput.next()) {
-                    usernameLabel.setText(queryOutput.getString("name"));
+                    usernameInput.setText(queryOutput.getString("name"));
                 }
 
                 conn.close();
@@ -43,7 +48,7 @@ public class PrimaryController {
         } catch (Exception e) {
             e.printStackTrace();
         }
+        ImageView imageView = new ImageView(new Image(getClass().getResource("/img/bg_principal.jpeg").toExternalForm()));
         App.setRoot("main_panell");
-
     }
 }
