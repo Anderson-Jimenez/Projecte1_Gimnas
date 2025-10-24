f42033eebc8e3a5c8d10734fb5696f57cb234b8f
roro333111 <arturbartres@gmail.com>
2025-10-24 10:30:00 +0200

    Visualitzacio de clases funcional + filtre de dia

--- src/main/java/com/mycompany/projecte1_gimnas/Edit_timetable_Controller.java
+++ src/main/java/com/mycompany/projecte1_gimnas/Edit_timetable_Controller.java
@@ -1,30 +1,38 @@
-    package com.mycompany.projecte1_gimnas;
+package com.mycompany.projecte1_gimnas;
 
-    import java.io.IOException;
-    import java.net.URL;
-    import java.sql.Connection;
-    import java.sql.DriverManager;
-    import java.sql.ResultSet;
-    import java.sql.SQLException;
-    import java.sql.Statement;
-    import java.util.HashSet;
-    import javafx.collections.FXCollections;
-    import javafx.collections.ObservableList;
+import java.io.IOException;
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.HashSet;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 
-    import javafx.event.ActionEvent;
-    import javafx.fxml.FXML;
-    import javafx.scene.control.Button;
-    import javafx.scene.control.TableColumn;
-    import javafx.scene.control.TableView;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
-    import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Text;
 
-    public class Edit_timetable_Controller {
+public class Edit_timetable_Controller {
 
-        @FXML
-        public void initialize() throws ClassNotFoundException{
-            Connection conn = DatabaseConnection.getConnection();
+    @FXML
+    public void initialize() throws ClassNotFoundException {
+        ObservableList<String> days = FXCollections.observableArrayList("dilluns", "dimarts", "dimecres", "dijous", "divendres", "dissabte");
 
+        day.setItems(days);
+        
+        Connection conn = DatabaseConnection.getConnection();
+        
         if (conn == null) {
             System.out.println("❌ No s'ha pogut connectar amb la base de dades.");
         }
@@ -34,29 +42,29 @@ import javafx.scene.control.cell.PropertyValueFactory;
         capacity.setCellValueFactory(new PropertyValueFactory<>("aforament"));
         start_time.setCellValueFactory(new PropertyValueFactory<>("start_time"));
         end_time.setCellValueFactory(new PropertyValueFactory<>("end_time"));
-        
+
         try {
-            
+
             Statement statement = conn.createStatement();
-            
+
             ObservableList<Clase> clases = FXCollections.observableArrayList();
-            
-            try{
+
+            try {
                 Statement stmt = conn.createStatement();
                 ResultSet rs = stmt.executeQuery("SELECT * FROM classes");
-                
+
                 while (rs.next()) {
-                    String name=rs.getString("name");
-                    int instructorT=rs.getInt("fk_id_instructor");
-                    int capacityT=rs.getInt("capacity");
-                    String date=rs.getString("date");
-                    String startT=rs.getString("start_time");
-                    String endT=rs.getString("end_time");
-                    
+                    String name = rs.getString("name");
+                    int instructorT = rs.getInt("fk_id_instructor");
+                    int capacityT = rs.getInt("capacity");
+                    String date = rs.getString("date");
+                    String startT = rs.getString("start_time");
+                    String endT = rs.getString("end_time");
+
                     clases.add(new Clase(name, instructorT, capacityT, date, startT, endT));
                 }
                 class_table.setItems(clases);
-                
+
             } catch (SQLException e) {
                 e.printStackTrace();
             }
@@ -66,9 +74,11 @@ import javafx.scene.control.cell.PropertyValueFactory;
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        
-    }        
- 
+
+    }
+
+    @FXML
+    private ComboBox<String> day;
 
     @FXML
     private Button assignInstructorsBtn;
@@ -110,6 +120,75 @@ import javafx.scene.control.cell.PropertyValueFactory;
     private TableColumn<Clase, String> start_time;
 
     @FXML
+    private TableColumn<?, ?> delete;
+
+    @FXML
+    private TableColumn<?, ?> edit;
+    
+    @FXML
+    private Button addClass;
+    
+    @FXML
+    private Text username;
+    
+    @FXML
+    private Text role;
+    
+    @FXML
+    void addClass(ActionEvent event) {
+        
+    }
+    
+    @FXML 
+    void updateTable(ActionEvent event) throws ClassNotFoundException{
+        String selected_day = day.getValue();
+        System.out.println(selected_day);
+        
+        Connection conn = DatabaseConnection.getConnection();
+        
+        if (conn == null) {
+            System.out.println("❌ No s'ha pogut connectar amb la base de dades.");
+        }
+
+        try {
+
+            Statement statement = conn.createStatement();
+
+            ObservableList<Clase> clases = FXCollections.observableArrayList();
+
+            try {
+                String sql="SELECT * FROM classes WHERE date= ?";
+                PreparedStatement stmt=conn.prepareStatement(sql);
+                stmt.setString(1,selected_day);
+                
+                ResultSet rs = stmt.executeQuery();
+
+                while (rs.next()) {
+                    String name = rs.getString("name");
+                    String instructor_query="SELECT name FROM instructor INNER JOIN classes ON instructor.ID = ?";
+                    int instructorT = rs.getInt("fk_id_instructor");
+                    int capacityT = rs.getInt("capacity");
+                    String date = rs.getString("date");
+                    String startT = rs.getString("start_time");
+                    String endT = rs.getString("end_time");
+
+                    clases.add(new Clase(name, instructorT, capacityT, date, startT, endT));
+                }
+                class_table.setItems(clases);
+
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+
+            conn.close();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+    
+    @FXML
     void assignInstructors(ActionEvent event) {
 
     }
@@ -122,7 +201,7 @@ import javafx.scene.control.cell.PropertyValueFactory;
     @FXML
     void editTimetable(ActionEvent event) throws IOException {
         App.setRoot("main_panell");
-        
+
     }
 
     @FXML
@@ -140,5 +219,4 @@ import javafx.scene.control.cell.PropertyValueFactory;
 
     }
 
-
-    }
+}
